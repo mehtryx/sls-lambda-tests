005c4fac27e4abcb93836a8c3edc7e2853ea31d0
Drew Edgar <drewedgar@ANCDEdgar.local>
2017-03-21 15:59:50 -0400

    schrodingers cat

--- test/bye.test.js
+++ test/bye.test.js
@@ -3,7 +3,7 @@ const expect = require( 'chai' ).expect;
 
 describe( 'bye', function() {
 	it('This is a test of tests. Good luck with that', function (done) {
-    var ayy = module.exports.frf;
+    var ayy = frf();
     expect( ayy ).to.eql( 'fmj' );
     done();
   });
