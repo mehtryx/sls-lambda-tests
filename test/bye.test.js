4cc43428b62a0073624e962de6088db0d041b19b
Drew Edgar <drewedgar@ANCDEdgar.local>
2017-03-21 16:08:10 -0400

    ... wedges

--- test/bye.test.js
+++ test/bye.test.js
@@ -1,10 +1,10 @@
 'use strict';
 const expect = require( 'chai' ).expect;
-const fmj = require( '../' );
+const fmj = require( '../bye.js' );
 
 describe( 'bye', function() {
 	it('This is a test of tests. Good luck with that', function (done) {
-    var ayy = fmj();
+    var ayy = fmj.frf();
     expect( ayy ).to.eql( 'fmj' );
     done();
   });
