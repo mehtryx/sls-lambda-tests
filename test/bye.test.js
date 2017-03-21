c55dab72e73b8ee47aac17bba064c15eec83af44
Drew Edgar <drewedgar@ANCDEdgar.local>
2017-03-21 15:46:17 -0400

    sigh

--- test/bye.test.js
+++ test/bye.test.js
@@ -4,7 +4,7 @@ const expect = require( 'chai' ).expect;
 describe( 'bye', function() {
 	it('This is a test of tests. Good luck with that', function (done) {
     var ayy = module.exports.frf;
-    expect( ayy.result ).to.eql( 'fmj' );
+    expect( ayy ).to.eql( 'fmj' );
     done();
   });
 });
