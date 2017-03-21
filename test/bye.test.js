12ae11526de2f41b4efc43948e60b5c110506fae
Drew Edgar <drewedgar@ANCDEdgar.local>
2017-03-21 14:58:42 -0400

    Mocha test... test?

---
+++ test/bye.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe( 'bye', function() {
+	it('This is a test of tests. Good luck with that', function (done) {
+    var onePlusOne = 1 + 1;
+    onePlusOne.should.equal(2);
+    done();
+  });
+};
