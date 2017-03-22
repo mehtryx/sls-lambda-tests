3ae2d3050fff7e795e192bd7d47b8befacc25cfb
Drew Edgar <drewedgar@ANCDEdgar.local>
2017-03-22 14:04:57 -0400

    The real FRF

--- test/bye.test.js
+++ test/bye.test.js
@@ -7,8 +7,13 @@ const fmj = require( '../bye.js' );
 
 describe( 'bye', function() {
 	it('This is a test of tests. Good luck with that', function (done) {
-    var ayy = fmj.frf();
-    expect( ayy ).to.eql( 'fmj' );
-    done();
+	    var ayy = fmj.frf();
+	    expect( ayy ).to.eql( 'fmj' );
+    	done();
   });
+	it('uh oh', function (done) {
+		var ayy = fmj.frf();
+    	expect( ayy ).to.eql( 'not fmj' )
+    	done();
+	});
 });
