b5eb7ace0ff303bb6e3a2338c59b55fdfacae4e8
Drew Edgar <drewedgar@ANCDEdgar.local>
2017-06-12 11:09:47 -0400

    demo update for test example

--- test/bye.test.js
+++ test/bye.test.js
@@ -5,10 +5,27 @@
 const expect = require( 'chai' ).expect;
 const fmj = require( '../bye.js' );
 
-describe( 'bye', function() {
+describe( 'Test Suite 1', function() {
 	it('This is a test of tests. Good luck with that', function (done) {
 	    var ayy = fmj.frf();
 	    expect( ayy ).to.eql( 'fmj' );
+	done();
+  });
+	it('bye again', function (done) {
+		var ayy = 'fmj';
+    	expect( ayy ).to.eql( 'fmj' )
+    	done();
+	});
+	it('A test name', function (done) {
+		var ayy = 'travis_eaten_by_suave_shark';
+    	expect( ayy ).to.eql( 'not_what_i_expected' )
+    	done();
+	});
+});
+describe( 'Test Suite 2', function() {
+	it('This is a failing test of tests. Good luck with that', function (done) {
+	    var ayy = fmj.frf();
+	    expect( ayy ).to.eql( 'jmf' );
 	done();
   });
 	it('bye again', function (done) {
@@ -16,9 +33,9 @@ describe( 'bye', function() {
     	expect( ayy ).to.eql( 'fmj' )
     	done();
 	});
-	it('3rd', function (done) {
-		var ayy = 'lmao';
-    	expect( ayy ).to.eql( 'lmao' )
+	it('Another test name', function (done) {
+		var ayy = 'spanish_inquisition';
+    	expect( ayy ).to.eql( 'also_not_what_i_expected' )
     	done();
 	});
 });
